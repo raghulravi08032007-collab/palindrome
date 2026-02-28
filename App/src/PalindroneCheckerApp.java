384392bc22d00689d7d555cda031f50e7037491f
raghulravi08032007 <raghulravi08032007@gmail.com>
2026-03-01 00:02:19 +0530

    UC2 Hardcoded Palindrome Implementation

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,12 +1,13 @@
-// version 1.0
+// version 2.0
 // author Abc
-// useCase 1: Welcome page
+// useCase 2:
 
 public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
 
     }
 }
